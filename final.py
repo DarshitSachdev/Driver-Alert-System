3600631cb49d48959fc3c88580337549d9277e28
DarshitSachdev <38495200+DarshitSachdev@users.noreply.github.com>
2020-03-03 00:46:57 +0530

    Commit

---
+++ final.py
@@ -0,0 +1,324 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 11 11:48:24 2019
+
+@author: darsh
+"""
+
+# USAGE
+# python detect_blinks.py --shape-predictor shape_predictor_68_face_landmarks.dat --video blink_detection_demo.mp4
+# python detect_blinks.py --shape-predictor shape_predictor_68_face_landmarks.dat
+
+# import the necessary packages
+from scipy.spatial import distance as dist
+from imutils.video import FileVideoStream
+from imutils.video import VideoStream
+from imutils import face_utils
+import numpy as np
+import argparse
+import imutils
+import time
+import dlib
+import cv2
+#import winsound
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+
+
+duration = 5000  # milliseconds
+freq = 440  # Hz
+
+def eye_aspect_ratio(eye):
+    # compute the euclidean distances between the two sets of
+    # vertical eye landmarks (x, y)-coordinates
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+
+    # compute the euclidean distance between the horizontal
+    # eye landmark (x, y)-coordinates
+    C = dist.euclidean(eye[0], eye[3])
+
+    # compute the eye aspect ratio
+    ear = (A + B) / (2.0 * C)
+
+    # return the eye aspect ratio
+    return ear
+
+def get_landmarks(im):
+    rects = detector(im, 1)
+
+    if len(rects) > 1:
+        return "error"
+    if len(rects) == 0:
+        return "error"
+    return np.matrix([[p.x, p.y] for p in predictor(im, rects[0]).parts()])
+
+
+def annotate_landmarks(im, landmarks):
+    im = im.copy()
+    for idx, point in enumerate(landmarks):
+        pos = (point[0, 0], point[0, 1])
+        cv2.putText(im, str(idx), pos,
+                    fontFace=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,
+                    fontScale=0.4,
+                    color=(0, 0, 255))
+        cv2.circle(im, pos, 3, color=(0, 255, 255))
+    return im
+
+
+def top_lip(landmarks):
+    top_lip_pts = []
+    for i in range(50, 53):
+        top_lip_pts.append(landmarks[i])
+    for i in range(61, 64):
+        top_lip_pts.append(landmarks[i])
+    top_lip_all_pts = np.squeeze(np.asarray(top_lip_pts))
+    top_lip_mean = np.mean(top_lip_pts, axis=0)
+    return int(top_lip_mean[:, 1])
+
+
+def bottom_lip(landmarks):
+    bottom_lip_pts = []
+    for i in range(65, 68):
+        bottom_lip_pts.append(landmarks[i])
+    for i in range(56, 59):
+        bottom_lip_pts.append(landmarks[i])
+    bottom_lip_all_pts = np.squeeze(np.asarray(bottom_lip_pts))
+    bottom_lip_mean = np.mean(bottom_lip_pts, axis=0)
+    return int(bottom_lip_mean[:, 1])
+
+def mouth_open(image):
+    landmarks = get_landmarks(image)
+
+    if landmarks == "error":
+        return image, 0
+
+    image_with_landmarks = annotate_landmarks(image, landmarks)
+    top_lip_center = top_lip(landmarks)
+    bottom_lip_center = bottom_lip(landmarks)
+    lip_distance = abs(top_lip_center - bottom_lip_center)
+    return image_with_landmarks, lip_distance
+
+# construct the argument parse and parse the arguments
+'''ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--shape-predictor", required=True, help="path to facial landmark predictor")
+ap.add_argument("-v", "--video", type=str, help="path to input video file")
+args = vars(ap.parse_args())'''
+
+# define two constants, one for the eye aspect ratio to indicate
+# blink and then a second constant for the number of consecutive
+# frames the eye must be below the threshold
+EYE_AR_THRESH = 0.25
+EYE_AR_CONSEC_FRAMES = 30
+
+# initialize the frame counters and the total number of blinks
+COUNTER = 0
+yawns = 0
+yawn_status = False
+TOTAL = 0
+
+# initialize dlib's face detector (HOG-based) and then create
+# the facial landmark predictor
+print("[INFO] loading facial landmark predictor...")
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+
+sender_email_id='carcamerasystem@gmail.com'
+receiver_email_id='carcamerasystem@gmail.com'
+sender_email_id_password='nisarg@123'
+
+    # Python code to illustrate Sending mail from 
+    # your Gmail account 
+import smtplib 
+
+#camera_port = 0
+#file = 'capture.jpeg'
+
+#camera = cv2.VideoCapture(camera_port)
+
+#ret,frame2 = camera.read()
+#cv2.imwrite(file, frame2)
+    # creates SMTP session 
+s = smtplib.SMTP('smtp.gmail.com', 587) 
+    
+    # start TLS for security 
+s.starttls() 
+    
+    # Authentication 
+s.login(sender_email_id, sender_email_id_password) 
+    
+    
+    # sending the mail 
+s.sendmail(sender_email_id, receiver_email_id, 'Subject: Your car is started. \n\n Somebody is using your car') 
+    
+    # terminating the session 
+#camera.release()
+s.quit() 
+
+
+# grab the indexes of the facial landmarks for the left and
+# right eye, respectively
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+# start the video stream thread
+print("[INFO] starting video stream thread...")
+#vs = FileVideoStream('blink_detection_demo.mp4').start()
+#fileStream = True
+vs = VideoStream(src=0).start()
+# vs = VideoStream(usePiCamera=True).start()
+fileStream = False
+time.sleep(1.0)
+
+# loop over frames from the video stream
+while True:
+    # if this is a file video stream, then we need to check if
+    # there any more frames left in the buffer to process
+    #if fileStream and not vs.more():
+    #    break
+
+    # grab the frame from the threaded video file stream, resize
+    # it, and convert it to grayscale
+    # channels)
+    
+    frame1 = vs.read()
+    image_landmarks, lip_distance = mouth_open(frame1)
+
+    #yawning
+    
+    prev_yawn_status = yawn_status
+
+    if lip_distance > 25:
+        yawn_status = True
+        if yawns >= 5:
+            cv2.putText(frame1, "Warning", (150, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+            winsound.Beep(freq, duration)
+        output_text = " Yawn Count: " + str(yawns + 1)
+        cv2.putText(frame1, output_text, (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 127), 2)
+
+    else:
+        yawn_status = False
+
+    if prev_yawn_status == True and yawn_status == False:
+        yawns += 1
+
+    cv2.imshow('Live Landmarks', image_landmarks)
+    cv2.imshow('Yawn Detection', frame1)
+    
+    #drowsiness
+    
+    frame = vs.read()
+    frame = imutils.resize(frame, width=450)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # detect faces in the grayscale frame
+    rects = detector(gray, 0)
+    temp = 1
+    # loop over the face detections
+    for rect in rects:
+        # determine the facial landmarks for the face region, then
+        # convert the facial landmark (x, y)-coordinates to a NumPy
+        # array
+        shape = predictor(gray,rect)
+        shape = face_utils.shape_to_np(shape)
+
+        # extract the left and right eye coordinates, then use the
+        # coordinates to compute the eye aspect ratio for both eyes
+        leftEye = shape[lStart:lEnd]
+        rightEye = shape[rStart:rEnd]
+        leftEAR = eye_aspect_ratio(leftEye)
+        rightEAR = eye_aspect_ratio(rightEye)
+
+        # average the eye aspect ratio together for both eyes
+        ear = (leftEAR + rightEAR) / 2.0
+
+        # compute the convex hull for the left and right eye, then
+        # visualize each of the eyes
+        leftEyeHull = cv2.convexHull(leftEye)
+        rightEyeHull = cv2.convexHull(rightEye)
+        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
+        # check to see if the eye aspect ratio is below the blink
+        # threshold, and if so, increment the blink frame counter
+        '''maxt = time.time()
+        maxt1 = maxt + 10
+        while time.time()<=maxt1:
+            if ear < EYE_AR_THRESH:
+                temp = 0
+                break
+            maxt = maxt+1
+
+        if temp==0: cv2.putText(frame,"Driver alert",(300,50),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)'''
+        if ear < EYE_AR_THRESH:
+            COUNTER = COUNTER+1
+            cv2.putText(frame,str(COUNTER)+"number frame",(300,80),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+            #cv2.putText(frame,int(EYE_AR_CONSEC_FRAMES),(300,50),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+        
+        elif ear > EYE_AR_THRESH:COUNTER = 0
+        # otherwise, the eye aspect ratio is not below the blink
+        # threshold
+        if COUNTER > EYE_AR_CONSEC_FRAMES:
+           cv2.putText(frame,"Driver alert",(300,100),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+           winsound.Beep(freq, duration)
+           break
+
+            # reset the eye frame counter
+
+        # draw the total number of blinks on the frame along with
+        # the computed eye aspect ratio for the frame
+        '''cv2.putText(frame, "Blinks: {}".format(TOTAL), (10, 30),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)'''
+        cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+    '''while True:
+        cv2.putText(frame,"Driver alert",(300,100),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break'''
+    
+    # show the frame
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
+# do a bit of cle
+
+    # cv2.imshow('Result', image_with_landmarks)
+    # cv2.imwrite('image_with_landmarks.jpg',image_with_landmarks)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+
+#cap = cv2.VideoCapture(0)
+#yawns = 0
+#yawn_status = False
+
+'''while True:
+    ret, frame = cap.read()
+    image_landmarks, lip_distance = mouth_open(frame)
+
+    prev_yawn_status = yawn_status
+
+    if lip_distance > 25:
+        yawn_status = True
+        if yawns >= 5:
+            cv2.putText(frame, "Warning", (150, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+        output_text = " Yawn Count: " + str(yawns + 1)
+        cv2.putText(frame, output_text, (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 127), 2)
+
+    else:
+        yawn_status = False
+
+    if prev_yawn_status == True and yawn_status == False:
+        yawns += 1
+
+    cv2.imshow('Live Landmarks', image_landmarks)
+    cv2.imshow('Yawn Detection', frame)'''
+
+vs.stop()
+cv2.destroyAllWindows()
